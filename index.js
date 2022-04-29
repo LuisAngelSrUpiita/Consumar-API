e7fcca3d303f558c36619594d81d794c940b0186
LASAR\Luis Angel <luisangel.sr@outlook.com>
2022-04-29 03:30:37 -0500

    Solucionado error CORS

--- index.js
+++ index.js
@@ -1,30 +1,23 @@
 const API_URL = "https://api.datos.gob.mx/v1/calidadAire";
 
-//const xhr = new XMLHttpRequest();
+const HTMLResponse = document.querySelector('.app');
 
-const HTMLResponse = document.querySelector("#app");
-
-fetch(`${API_URL}/users`)
+fetch(API_URL)
     .then ((response) => response.json())
-    .then((users) => {
-        const tpl = data.map(()=> `<li>${users.name}</li>`);
-        HTMLResponse.innerHTML = `<ul>${tpl}</ul>`;
+    .then(data => {
+            const p1 = document.createElement('p1')
+            p1.innerHTML = data.results[0].stations[0].name
+            HTMLResponse.appendChild(p1)
+
+            const p = document.createElement('p')
+            p.innerHTML = data.results[0].stations[0].indexes[0].scale
+            HTMLResponse.appendChild(p)
+
+            const p2 = document.createElement('p2')
+            p2.innerHTML = data.results[0].stations[0].value
+            HTMLResponse.appendChild(p2)
+
+            console.log(data)
     })
-   
+    .catch(err => console.log(err))
 
-
-/*
-function onRequestHandler(){
-    if(this.readyState == 4 && this.status == 200){
-        const data = JSON.parse(this.response);
-        const HTMLResponse = document.querySelector("#app");
-
-        const tpl = data.map(()=> `<li>${user.name}</li>`);
-        HTMLResponse.innerHTML = `<ul>${tpl}</ul>`;
-    }
-}
-
-xhr.addEventListener("load", onRequestHandler);
-xhr.open('GET', '${API_URL}');
-xhr.send(); 
- */
